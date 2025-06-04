bd99c05d290004e514417018c847ad9ffb20384c
sehan29 <sehangamage708@gmail.com>
2025-06-04 18:09:48 +0530

    Socket By Pass

--- app.py
+++ app.py
@@ -234,4 +234,4 @@ def logout():
  
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
